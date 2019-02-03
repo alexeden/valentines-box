be5856574eda18890b79478c431106f73aa48000
Alex Eden <alexandereden91@gmail.com>
2019-02-02 19:48:07 -0600

    Setup the logic for detecting open/closed state

--- src/main.cc
+++ src/main.cc
@@ -1,32 +1,54 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-// #include "utility/Adafruit_MS_PWMServoDriver.h"
+
+static const uint8_t POT_NEG 	= A2; // orange
+static const uint8_t POT_WIPER 	= A3; // purple
+static const uint8_t POT_POS 	= A4; // yellow
+static const uint8_t MOTOR		= 4;
+
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *motor = AFMS.getMotor(4);
+Adafruit_DCMotor *motor = AFMS.getMotor(MOTOR);
 
 void setup() {
-	Serial.begin(115200); // set up Serial library at 9600 bps
-	Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+	Serial.begin(115200);
 
-	AFMS.begin(); // create with the default frequency 1.6KHz
+	// AFMS.begin();
 
 	// Set the speed to start, from 0 (off) to 255 (max speed)
-	motor->setSpeed(255);
-	motor->run(FORWARD);
+	// motor->setSpeed(255);
+	// motor->run(FORWARD);
 	// turn on motor
-	motor->run(RELEASE);
+	// motor->run(RELEASE);
 }
 
+// uint16_t wiper = 0x00;
+
 void loop() {
-	uint8_t i;
+	uint16_t neg_rail = analogRead(POT_NEG);
+	uint16_t wiper = analogRead(POT_WIPER);
+	uint16_t pos_rail = analogRead(POT_POS);
 
-	Serial.print("tick");
+	float ratio = ((float)wiper - (float)neg_rail) / ((float)pos_rail - (float)neg_rail);
+	// wiper = analogRead(A2);
 
-	motor->run(FORWARD);
+	Serial.print("ratio: ");
+	Serial.println(ratio);
+	// Serial.print("\ta2: ");
+	// Serial.print(a2);
+	// Serial.print("\ta3: ");
+	// Serial.print(a3);
+	// Serial.print("\ta4: ");
+	// Serial.println(a4);
+
+	// Serial.println(wiper);
+
+	// Serial.print("tick");
+
+	// motor->run(FORWARD);
 	// for (i = 0; i < 255; i++) {
 	// 	motor->setSpeed(i);
 	// 	delay(10);
@@ -35,10 +57,10 @@ void loop() {
 	// 	motor->setSpeed(i);
 	// 	delay(10);
 	// }
-	delay(3000);
-	Serial.print("tock");
+	delay(500);
+	// Serial.print("tock");
 
-	motor->run(BACKWARD);
+	// motor->run(BACKWARD);
 	// for (i = 0; i < 255; i++) {
 	// 	motor->setSpeed(i);
 	// 	delay(10);
@@ -47,8 +69,8 @@ void loop() {
 	// 	motor->setSpeed(i);
 	// 	delay(10);
 	// }
-	delay(3000);
-	Serial.print("tech");
-	motor->run(RELEASE);
-	delay(3000);
+	// delay(3000);
+	// Serial.print("tech");
+	// motor->run(RELEASE);
+	// delay(3000);
 }
