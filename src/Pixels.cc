79ea9f6fbdc03bccb4c7292d708f00a4460fec47
Alex Eden <alexandereden91@gmail.com>
2019-02-04 23:53:56 -0600

    Lights work!

---
+++ src/Pixels.cc
@@ -0,0 +1,124 @@
+#ifndef __Pixels__
+#define __Pixels__
+
+#include <Wire.h>
+#include <Streaming.h>
+#include <Adafruit_NeoPixel.h>
+
+class Pixels {
+
+private:
+	const float DEGRADE = 0.97;
+    const uint8_t PIN, N;
+	Adafruit_NeoPixel *strip;
+	int8_t x;
+	int8_t v;
+	uint32_t *pixels;
+	uint32_t disp_mark;
+
+	void rotate() {
+		while (x <= 0) {
+			x += N;
+		}
+		while (x >= N) {
+			x -= N;
+		}
+	}
+
+	void reverse() {
+		if (x > N || x < 0) {
+			v *= -1;
+		}
+	}
+
+	void degrade(uint32_t &pixel) {
+		pixel = (
+			0x00 << 24 |
+			uint32_t(float(pixel >> 16 & 0xFF) * DEGRADE) << 16 |
+			uint32_t(float(pixel >> 8 & 0xFF) * DEGRADE) << 8 |
+			uint32_t(float(pixel & 0xFF) * DEGRADE)
+		);
+	}
+
+public:
+    Pixels (uint8_t _pin, uint8_t _n)
+	: 	PIN(_pin),
+		N(_n),
+		strip(new Adafruit_NeoPixel(_n, _pin, NEO_GRB + NEO_KHZ800)),
+		x(0.0),
+		v(0.1)
+	{}
+
+	void begin() {
+		pinMode(PIN, OUTPUT);
+		strip -> begin();
+		strip -> show();
+		pixels = new uint32_t[N];
+		clear_pixels();
+		disp_mark = micros();
+	}
+
+	void clear_pixels() {
+		for (int i = 0; i < N; i++) {
+			pixels[i] = 0x00000000;
+		}
+	}
+
+	// void set_speed(u)
+
+	void update() {
+		// pull = p;
+		// if(pull < 0) {
+		// 	min_pull = min(min_pull, sqrt(pull));
+		// }
+		// else if(pull > 0) {
+		// 	max_pull = max(max_pull, sqrt(pull));
+		// }
+		// pull_force += p;
+		// // Degrade the pull force
+		// if(pull == 0) pull_force *= 0.1;
+		// float net = 0;
+		// float drag = abs(v);
+		// drag *= v > 0 ? -1 : 1;
+		// net += drag;
+		// net += pull_force;
+		// net /= MASS;
+		// a = net;
+		// v = 100;
+		x += v;
+		reverse();
+	}
+
+	int get_x() {
+		return (int)x;
+	}
+
+	void display() {
+		uint32_t bgr = 0x00FF0055;
+		// if (pull > 0) {
+		// 	bgr = 0x00FF0000;
+		// }
+		// else if (pull < 0) {
+		// 	bgr = 0x000000FF;
+		// }
+		// else {
+		// 	bgr = 0x00FF00FF;
+		// }
+		pixels[int(x)] = bgr;
+		for (int i = 0; i < N; i++) {
+			if (i == int(x)) {
+				strip -> setPixelColor(i, pixels[i]);
+			}
+			else {
+				degrade(pixels[i]);
+				strip -> setPixelColor(i, pixels[i]);
+			}
+		}
+		strip -> show();
+
+		disp_mark = micros();
+	}
+
+};
+
+#endif
--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,7 @@
 #include <Streaming.h>
 #include <Wire.h>
 #include "Actuator.cc"
-#include "Neopixels.cc"
+#include "Pixels.cc"
 
 Actuator actuator(
 	ACT_POT_NEG_PIN,
@@ -14,20 +14,36 @@ Actuator actuator(
 	MOTOR_NUM
 );
 
-Adafruit_SSD1306 display(128, 32, &Wire);
-Neopixels pixels(NEOPIXEL_PIN);
+Adafruit_SSD1306 *display = new Adafruit_SSD1306(128, 32, &Wire);
+Pixels *pixels = new Pixels(NEOPIXEL_PIN, 20);
 
 void setup() {
 	Serial.begin(115200);
 	actuator.begin();
-	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-	display.clearDisplay();
-	display.display();
+	pixels -> begin();
+	display -> begin(SSD1306_SWITCHCAPVCC, 0x3C);
+	display -> clearDisplay();
+	display -> setTextSize(2);      // Normal 1:1 pixel scale
+	display -> setTextColor(WHITE); // Draw white text
+	// display -> setCursor(0, 0);     // Start at top-left corner
+	// display -> cp437(true);         // Use full 256 char 'Code Page 437' font
+	// display -> println("HIIIIIII");
+	// display -> display();
 }
 
+int i = 0;
+
 void loop() {
-	actuator.open();
-	delay(2000);
-	actuator.close();
-	delay(2000);
+	pixels -> update();
+	pixels -> display();
+	display -> clearDisplay();
+	display -> setCursor(0, 0);
+	// display -> println(++i);
+	display -> println(pixels->get_x());
+	display -> display();
+	delay(10);
+	// // actuator.open();
+	// delay(2000);
+	// actuator.close();
+	// delay(2000);
 }
